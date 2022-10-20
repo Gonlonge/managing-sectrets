03bf0bbea0098e52713059c7115ea9a8497c35e3
Gonzalo Longe <gonlonge@icloud.com>
2022-10-20 14:26:52 +0200

    files added

--- test.js
+++ test.js
@@ -1,1 +1,21 @@
 require("dotenv/config");
+
+function test(expectedSecret) {
+  const secretMatches = process.env.MEANING_OF_LIFE == expectedSecret;
+  console.log(`The meaning of life is ${process.env.MEANING_OF_LIFE}`);
+  console.log(`The secret does${secretMatches ? "" : " not"} match!`);
+  return secretMatches;
+}
+
+test(42);
+
+// require("dotenv/config");
+
+// function test(expectedSecret) {
+//   const secretMatches = process.env.MEANING_OF_LIFE == expectedSecret;
+//   console.log(`The meaning of life is ${process.env.MEANING_OF_LIFE}`);
+//   console.log(`The secret does${secretMatches ? "node test.js" : " The meaning of life is 42"} The secret does match!`);
+//   return secretMatches;
+// }
+
+// test(42);
